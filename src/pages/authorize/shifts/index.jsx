6b63dccd0e419580fd8a3e0a4bdd9f81880bd5ed
Naufal Dinta <naufaldintaa@gmail.com>
2025-07-30 21:23:28 +0700

    list sesi date and time

--- src/pages/authorize/shifts/index.jsx
+++ src/pages/authorize/shifts/index.jsx
@@ -159,7 +159,9 @@ const ShiftScreen = () => {
                         {item?.cashier?.name}
                       </div>
                       <div className="text-base-300 text-xs">
-                        {dateFormat(item?.transaction_date, 'DD/MM/YYYY')}
+                        {dateFormat(item?.transaction_date, 'DD/MM/YYYY')} {' '}
+                        {dateFormat(item?.started_at, 'HH:mm')} -{' '}
+                        {dateFormat(item?.finished_at, 'HH:mm', '(ongoing)')}
                       </div>
                     </div>
                   </div>
